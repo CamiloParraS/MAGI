bf2bfab6163eebedb067b2e2324e1ecf6782a3d8
Camilo Parra <juanparracsfn@gmail.com>
2026-09-16 01:15:17 -0500

    test(fixtures): add real-file edge-case corpus (empty/huge/deep)

---
+++ fixtures/corpus/edge/deep_real/Tercer Semestre/Fisica/asuntosSinImportancia/solarcalculator/solarcalculator/src/main/java/com/example/solarcalculator/model/calculateRequest.java
@@ -0,0 +1,10 @@
+package com.example.solarcalculator.model;
+
+public class calculateRequeste {
+
+    public double consumoMensual;
+    public double irradiacionSolar;
+    public double potenciaPanel;
+    public double costoKwh;
+    public double costoSistema;
+}
